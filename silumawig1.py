8336113afb1e9d66a685b72b1ae987886a17d831
Jay-Anne Baga Bulauan <78282096+jaywithannE@users.noreply.github.com>
2023-08-27 21:29:22 +0800

    Add files via upload

---
+++ silumawig1.py
@@ -0,0 +1,50 @@
+'''Compute the dimension 0 shifted bottleneck distance between two persistence 
+    diagrams X and Y where |X|=|Y|.
+'''
+
+import numpy as np
+
+
+
+
+def silumawig1(X,Y):
+    X=-np.sort(-X)
+    Y=-np.sort(-Y)
+    d=0
+    shift=0
+    
+    N = min(len(X),len(Y))
+    if N==1:
+        d=abs(X[0]-Y[0])/2
+        shift=d
+        return d,shift
+    X=X[0:N]
+    Y=Y[0:N]
+    Z = abs(X-Y)
+    l = np.argmax(Z)
+
+
+
+    if Y[l]>=X[l]:
+        X_copy=X
+        X=Y
+        Y=X_copy
+    if len(np.where(X>=Y)[0])==N:
+        d=(X[l]-Y[l])/2
+        shift=d
+        return d,shift
+            
+    else: 
+        m=np.argmin(X-Y)
+#        if  X[l]-Y[l]>X[m] and m<l:
+        if Y[m] - X[m] > Y[l] and m<l: 
+            return silumawig1(X[0:l],Y[0:l])
+#        elif Y[m]-X[m]<=Y[l] and m>l:
+        elif X[l] - Y[l] > X[m] and m>l:
+            return silumawig1(X[0:m],Y[0:m])
+        else:
+            d=(X[l]-Y[l]-X[m]+Y[m])/2
+            shift=(X[l]-Y[l]+X[m]-Y[m])/2
+            return d,shift           
+
+    
